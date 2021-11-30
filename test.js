a7a9fb8a2618b4772970019ac94568745f265559
Sandipan Das <61842142+sandip2224@users.noreply.github.com>
2021-11-30 09:19:04 +0530

    Delete test.js

--- test.js
+++
@@ -1,6 +0,0 @@
-let date = new Date()
-var y = date.toLocaleString('en-US', { month: '2-digit' })
-var x = date.toLocaleString('en-US', { day: '2-digit' })
-var z = date.getFullYear()
-var dateVal = x + "/" + y + "/" + z
-console.log(dateVal)
