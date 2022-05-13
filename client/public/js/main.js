8a7736b9bee4169448d831766a5105f71286293d
sandip2224 <sandipan2224@gmail.com>
2022-05-13 15:27:47 +0530

    [Feature] Added push notification support

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -3,7 +3,6 @@ const msg = document.getElementById('msg')
 const chatDiv = document.querySelector('.chat-messages')
 const usersList = document.getElementById('users')
 
-const roomname = document.getElementById('room-name').innerText
 const username = document.getElementById('user-name').innerHTML
 const room = document.getElementById('room-name').innerHTML
 
@@ -13,47 +12,58 @@ const socket = io()
 socket.emit('joinRoom', { username, room })
 
 socket.on('roomUsers', ({ room, users }) => {
-    outputRoomUsers(room, users)
+	outputRoomUsers(room, users)
 })
 
 // Message submit from client
 chatForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    socket.emit('chatMessage', msg.value)
-    msg.value = ''
-    msg.focus();
+	e.preventDefault()
+	socket.emit('chatMessage', msg.value)
+	msg.value = ''
+	msg.focus()
 })
 
 //  Receiving message from server
 socket.on('message', (msg) => {
-    displayMessage(msg)
-    chatDiv.scrollTop = chatDiv.scrollHeight;
+	displayMessage(msg)
+	if (msg.username !== 'Admin') {
+		fetch('http://localhost:3000/send-notification', {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: [
+				JSON.stringify({ msg: msg })
+			]
+		})
+	}
+	chatDiv.scrollTop = chatDiv.scrollHeight;
 })
 
 socket.on('type', (res) => {
-    document.getElementById('typing').innerHTML = res
+	document.getElementById('typing').innerHTML = res
 })
 
 function displayMessage(msg) {
-    const div = document.createElement('div');
-    div.classList.add('message')
-    div.innerHTML = `
+	const div = document.createElement('div');
+	div.classList.add('message')
+	div.innerHTML = `
         <p class="meta">${msg.username}<span>&nbsp;&nbsp;&nbsp;&nbsp;${msg.time}</span></p>
 	    <p class="text">${msg.text}</p>
     `
-    chatDiv.appendChild(div)
+	chatDiv.appendChild(div)
 }
 
 function outputRoomUsers(room, users) {
-    document.getElementById('room-name').innerText = room;
-    usersList.innerHTML = `
+	document.getElementById('room-name').innerText = room;
+	usersList.innerHTML = `
         ${users.map(user => `<li>${user.username}</li>`).join('')}
         `;
 }
 
 function onkeyFn() {
-    socket.emit('starttype', `${username} is typing...`)
-    setTimeout(() => {
-        socket.emit('stoptype', "")
-    }, 2000);
+	socket.emit('starttype', `${username} is typing...`)
+	setTimeout(() => {
+		socket.emit('stoptype', "")
+	}, 2000)
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path')
 const http = require('http')
 const colors = require("colors")
 const socketio = require('socket.io')
+const webpush = require('web-push')
 require("dotenv").config({ path: "./.env" })
 
 const formatMessage = require('./client/utils/messages')
@@ -14,6 +15,15 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+const vapidKeys = {
+	publicKey: process.env.PUBLIC_KEY,
+	privateKey: process.env.PRIVATE_KEY
+}
+
+let subscriptions = []
+
+webpush.setVapidDetails('mailto:test@test.com', vapidKeys.publicKey, vapidKeys.privateKey)
+
 connectDB()
 
 app.set('view engine', 'ejs')
@@ -21,73 +31,101 @@ app.set('views', path.join(__dirname, 'client/views'));
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
 app.use(express.static(path.join(__dirname, 'client/public')))
 app.use('/', require('./server/routes/chatRoute'))
 
+app.post('/register-push-device', (req, res) => {
+	console.log('Saving subscription...')
+	// console.log(req.body.name)
+	// console.log(req.body.room)
+	// console.log(req.body.subscription)
+	subscriptions.push({
+		subscription: req.body.subscription,
+		name: req.body.name,
+		room: req.body.room
+	})
+	console.log(subscriptions.length)
+	res.end()
+})
+
+app.post('/send-notification', (req, res) => {
+	console.log('Sending notification...')
+	const notifBody = req.body.msg.text
+	console.log(notifBody)
+	subscriptions.forEach((item) => {
+		if (item.room === req.body.msg.room) {
+			webpush.sendNotification(item.subscription, notifBody).catch((ex) => {
+				console.log(ex)
+			})
+		}
+	})
+	res.end()
+})
+
 // Run when client connects
 io.on('connection', (socket) => {
+	socket.on('joinRoom', ({ username, room }) => {
+		const user = userJoin(socket.id, username, room)
 
-    socket.on('joinRoom', ({ username, room }) => {
-        const user = userJoin(socket.id, username, room)
+		// Pushes a user socket to the given room
+		socket.join(user.room)
 
-        // Pushes a user socket to the given room
-        socket.join(user.room)
+		// Welcome adminMessage for client alone who connects
+		socket.emit('message', formatMessage('Admin', `Welcome to ChatIO ${user.username}!!`))
 
-        // Welcome adminMessage for client alone who connects
-        socket.emit('message', formatMessage('Admin', `Welcome to ChatIO ${user.username}!!`))
+		// Broadcast to everyone else when user connects
+		socket.broadcast.to(user.room).emit('message', formatMessage('Admin', `${user.username} has joined the chat!!`))
 
-        // Broadcast to everyone else when user connects
-        socket.broadcast.to(user.room).emit('message', formatMessage('Admin', `${user.username} has joined the chat!!`))
+		// Triggered during keypress event on client side
+		socket.on('starttype', (msg) => {
+			socket.broadcast.to(user.room).emit('type', msg)
+		})
 
-        // Triggered during keypress event on client side
-        socket.on('starttype', (msg) => {
-            socket.broadcast.to(user.room).emit('type', msg)
-        })
+		socket.on('stoptype', (msg) => {
+			socket.broadcast.to(user.room).emit('type', msg)
+		})
 
-        socket.on('stoptype', (msg) => {
-            socket.broadcast.to(user.room).emit('type', msg)
-        })
+		// Send users and room info to every client in a room
+		io.to(user.room).emit('roomUsers', {
+			room: user.room,
+			users: getRoomUsers(user.room)
+		})
+	})
 
-        // Send users and room info to every client in a room
-        io.to(user.room).emit('roomUsers', {
-            room: user.room,
-            users: getRoomUsers(user.room)
-        })
-    })
+	// Listen for chat message from client
+	socket.on('chatMessage', async (msg) => {
+		const user = getCurrentUser(socket.id)
+		const frmtMsg = formatMessage(user.username, msg, user.room)
+		const obj = {
+			username: `${frmtMsg.username}`,
+			time: `${frmtMsg.time} (${frmtMsg.date})`,
+			message: `${frmtMsg.text}`,
+			room: `${user.room}`
+		}
+		const newMsg = new chatModel(obj)
+		await newMsg.save()
+		io.to(user.room).emit('message', frmtMsg)
+	})
 
-    // Listen for chat message from client
-    socket.on('chatMessage', async (msg) => {
-        const user = getCurrentUser(socket.id)
-        const frmtMsg = formatMessage(user.username, msg)
-        const obj = {
-            username: `${frmtMsg.username}`,
-            time: `${frmtMsg.time} (${frmtMsg.date})`,
-            message: `${frmtMsg.text}`,
-            room: `${user.room}`
-        }
-        const newMsg = new chatModel(obj)
-        await newMsg.save()
-        io.to(user.room).emit('message', frmtMsg)
-    })
-
-    // Run when client disconnects
-    socket.on('disconnect', () => {
-        const user = userLeave(socket.id)
-        if (user) {
-            io.to(user.room).emit('message', formatMessage('Admin', `${user.username} has left the chat`))
-            // Send users and room info to every client
-            io.to(user.room).emit('roomUsers', {
-                room: user.room,
-                users: getRoomUsers(user.room)
-            })
-        }
-    })
+	// Run when client disconnects
+	socket.on('disconnect', () => {
+		const user = userLeave(socket.id)
+		if (user) {
+			io.to(user.room).emit('message', formatMessage('Admin', `${user.username} has left the chat`))
+			// Send users and room info to every client
+			io.to(user.room).emit('roomUsers', {
+				room: user.room,
+				users: getRoomUsers(user.room)
+			})
+		}
+	})
 })
 
 const conn = server.listen(process.env.PORT || 3000, console.log(`Server running on port ${process.env.PORT || 3000}`))
 
 // Handle unhandled promise rejections
 process.on("unhandledRejection", (err, promise) => {
-    console.log(`Error: ${err.message}`.red)
-    conn.close(() => process.exit(1))
+	console.log(`Error: ${err.message}`.red)
+	conn.close(() => process.exit(1))
 })
