8c8c5acb7f4e4463af33e7655914c434a9884a5f
sandip2224 <sandipan2224@gmail.com>
2022-07-24 14:46:19 +0530

    Updated CSS

--- client/public/js/worker.js
+++ client/public/js/worker.js
@@ -1,6 +1,5 @@
 self.addEventListener('push', event => {
 	const values = JSON.parse(event.data.text())
-	console.log(values)
 	self.registration.showNotification(values.user, {
 		body: values.msg
 	})
